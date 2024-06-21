f46de87f96786281c2b400bbaaaa3271a97257c4
Punit Parmar <152812841+Punit231@users.noreply.github.com>
2024-06-21 14:30:26 +0000

    datatypes

---
+++ 01_basics/data_types.js
@@ -0,0 +1,21 @@
+"use strict";     // treat all js code as new version
+
+// alert(3+3)     // we are using nodejs, not browser
+
+let name = "Punit"
+let age = 19
+let isLoggedIn = true
+
+// number
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value      // null is object in js
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Punit");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "punit123@gmail.com"
+var accountPassword = "123456789"
+accountCity = "Upleta"
+
+// accountId = 2                 // Not Allowed
+accountEmail = "abc@gmail.com"
+accountPassword = "987654321"
+accountCity = "Rajkot"
+
+// prefer not to use var
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
