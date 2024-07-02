e700a48327824332a6ac712c28ed5695ed690e99
Punit Parmar <152812841+Punit231@users.noreply.github.com>
2024-07-02 16:08:26 +0000

    switch case done

---
+++ 04_Control_Flow/02_switch.js
@@ -0,0 +1,21 @@
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+        
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Case Match");
+        break;
+}
