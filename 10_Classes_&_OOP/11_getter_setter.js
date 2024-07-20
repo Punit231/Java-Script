21fb50fb50fdddfe89c1a9fbf277190fd686559b
Punit Parmar <152812841+Punit231@users.noreply.github.com>
2024-07-20 16:42:27 +0000

    getter & setter

---
+++ 10_Classes_&_OOP/11_getter_setter.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toLowerCase()
+    }
+
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}Punit`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const Punit = new User("punit@Punit.ai", "abc")
+console.log(Punit.email);
+console.log(Punit.password);
